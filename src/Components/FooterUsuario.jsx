1fd4e19e300fb2be7443a4f86cb5dcb0ddb61862
anakarinabr <ana.blanco@correo.unimet.edu.ve>
2024-03-27 13:00:05 -0400

    Footer ready

--- src/Components/FooterUsuario.jsx
+++ src/Components/FooterUsuario.jsx
@@ -11,32 +11,37 @@ export default function FooterUsuario() {
   return (
     <div className={styles.All}>
       <div className={styles.Container}>
+        <div className={styles.Option}>
+          <label id={styles.p}>
+            <div className={styles.Referencias}>
+              <div className={styles.Column}>
+                <a href="https://www.instagram.com/unimet/?hl=es">
+                  <img src="/ig.png" className={styles.Icons} />
+                </a>
+              </div>
+              <div className={styles.Column}>
+                <a href="https://www.facebook.com/unimet/?locale=es_LA">
+                  <img src="/fb.png" className={styles.Icons} />
+                </a>
+              </div>
+              <div className={styles.Column}>
+                <a href="https://twitter.com/Unimet?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
+                  <img src="/x.png" className={styles.Icons} />
+                </a>
+              </div>
+            </div>
+          </label>
+        </div>
+
         <div className={styles.Row}>
           <div className={styles.logos}>
             <a href="https://www.unimet.edu.ve/">
               <img src="/logo.png" className={styles.Image} />
             </a>
           </div>
-          <div className={styles.Referencias}>
-          <div className={styles.Column}>
-              <a href="https://www.instagram.com/unimet/?hl=es">
-                <img src="/ig.png" className={styles.Icons} />
-              </a>
-          </div>
-          <div className={styles.Column}>
-              <a href="https://www.facebook.com/unimet/?locale=es_LA">
-                <img src="/fb.png" className={styles.Icons} />
-              </a>
-          </div>
-          <div className={styles.X} >
-              <a href="https://twitter.com/Unimet?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">
-                <img src="/x.png" className={styles.Icons} id={styles.x}/>
-              </a>
-          </div>
-          </div>
         </div>
+
         <div className={styles.Pie}>
-          <hr />
           <div className={styles.Row}>
             &copy;{new Date().getFullYear()} Universidad Metropolitana | Todos
             los derechos reservados.
