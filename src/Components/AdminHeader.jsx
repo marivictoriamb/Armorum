089f7a6e95f0329edf6f358daa89744ec5f92095
mojeda20 <141047560+moisesliotawork@users.noreply.github.com>
2024-03-27 11:10:49 -0400

    Perfil de Admin
    Crear Perfil de Admin y añadir perfil de admin al Header de la pagina de Admin

--- src/Components/AdminHeader.jsx
+++ src/Components/AdminHeader.jsx
@@ -1,23 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./AdminHeader.module.css"
+import styles from "./AdminHeader.module.css";
 
 export default function AdminHeader() {
-
-  const [clicked, setClicked] = useState(false)
+  const [clicked, setClicked] = useState(false);
   const navigate = useNavigate();
 
   const handleNavigate = (name) => () => {
     navigate(name);
-  }
+  };
 
   return (
-      <div style={{position:"sticky", width:"100%"}} className={`${styles.All} ${styles.OrangeLine}`}>
-        <img src="/Tree.png"  onClick={handleNavigate("/landing")}  className={styles.Img} style={{textAlign: 'left', width: '200px', marginLeft:"2vw"}}/>
-        <img src="/name.png"  onClick={handleNavigate("/landing")}  className={styles.Img} style={{textAlign: 'left', width: '280px', paddingLeft:"1vw", height:"170px"}}/>
-        <img src="/profileHeader.png" onClick={handleNavigate("/perfil")} className={styles.Img} style={{cursor:"pointer", textAlign: 'right', width: '180px', marginLeft:"60%", height:"180px"}}/>
+    <div
+      style={{ position: "sticky", width: "100%" }}
+      className={`${styles.All} ${styles.OrangeLine}`}
+    >
+      <img
+        src="/Tree.png"
+        onClick={handleNavigate("/landing")}
+        className={styles.Img}
+        style={{ textAlign: "left", width: "200px", marginLeft: "2vw" }}
+      />
+      <img
+        src="/name.png"
+        onClick={handleNavigate("/landing")}
+        className={styles.Img}
+        style={{
+          textAlign: "left",
+          width: "280px",
+          paddingLeft: "1vw",
+          height: "170px",
+        }}
+      />
+      <div className={styles.Links}>
+        <img
+          src="/profileHeader.png"
+          onClick={handleNavigate("/perfil")}
+          className={styles.Profile}
+        />
       </div>
-  )
+    </div>
+  );
 }
 /*
 <div className={styles.Links}>
