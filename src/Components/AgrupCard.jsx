70687a4bc7ba7d8125f5548141bc7c823198573a
anakarinabr <ana.blanco@correo.unimet.edu.ve>
2024-03-28 11:40:01 -0400

    AgrupCard & CategoryCard

--- src/Components/AgrupCard.jsx
+++ src/Components/AgrupCard.jsx
@@ -14,13 +14,12 @@ export default function AgrupCard({name, description, category}){
             <div className={styles.Card}>
                 <div className={styles.banner}>
                     <div className={styles.Image} onClick={handleClick} style={{cursor:"pointer"}}>
-                        <img style={{margin:"auto", width: "300px", height:"50%", borderRadius:"2rem", objectFit:"contain"}} alt="control" src="/panda.png"/>
+                        <img style={{margin:"auto", width: "300px", height:"50%", borderRadius:"100%", objectFit:"cover"}} alt="control" src="/panda.png"/>
                     </div>
                 </div>
-                <br />
-                <div className={styles.menu} >
+                <div className={styles.Agrupaciones} >
                     <h1 className={styles.Name}>{name}</h1>
-                    <img onClick={handleClick}src="https://cdn-icons-png.flaticon.com/512/6324/6324826.png" style={{cursor:"pointer", width: "3rem", height: "3rem"}}/>
+                    <img onClick={handleClick}src="https://cdn-icons-png.flaticon.com/512/6324/6324826.png" style={{cursor:"pointer", width: "2rem", height: "2rem"}}/>
                 </div>
             </div>
         </div>
--- src/Components/CategoryCard.jsx
+++ src/Components/CategoryCard.jsx
@@ -1,28 +1,44 @@
 import { useNavigate } from "react-router-dom";
-import styles from './AgrupCard.module.css'
+import styles from "./AgrupCard.module.css";
 
-export default function CategoryCard({name}){    
-    const navigate = useNavigate();
+export default function CategoryCard({ name }) {
+  const navigate = useNavigate();
 
-    const handleClick = () => {
-        navigate(`/categorias/${name}`);
-      };
+  const handleClick = () => {
+    navigate(`/categorias/${name}`);
+  };
 
-
-    return(
-        <div className={styles.All} >
-            <div className={styles.Card}>
-                <div className={styles.banner}>
-                    <div className={styles.Image} onClick={handleClick} style={{cursor:"pointer"}}>
-                        <img style={{margin:"auto", width: "300px", height:"50%", borderRadius:"2rem", objectFit:"contain"}} alt="control" src="/panda.png"/>
-                    </div>
-                </div>
-                <br />
-                <div className={styles.menu}>
-                    <h2 className={styles.Name}>{name}</h2>
-                    <button style={{backgroundColor:"orange", padding: '5px 10px'}}>Desactivar</button>
-                </div>
-            </div>
+  return (
+    <div className={styles.All}>
+      <div className={styles.Card}>
+        <div className={styles.banner}>
+          <div
+            className={styles.Image}
+            onClick={handleClick}
+            style={{ cursor: "pointer" }}
+          >
+            <img
+              style={{
+                margin: "auto",
+                width: "300px",
+                height: "50%",
+                borderRadius: "100%",
+                objectFit: "contain",
+              }}
+              alt="control"
+              src="/panda.png"
+            />
+          </div>
         </div>
-    )
+        <br />
+        <div className={styles.menu}>
+            <h2 className={styles.Name}>{name}</h2>
+            <button style={{ backgroundColor: "orange", padding: "5px 10px" }}>
+              Desactivar
+            </button>
+         
+        </div>
+      </div>
+    </div>
+  );
 }
